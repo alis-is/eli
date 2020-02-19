fcbceac8929c263a3b0230523f5b44120d634d0b
cryi <cryi@tutanota.com>
2020-02-19 17:16:24 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 LATEST=$(curl -sL https://api.github.com/repos/cryon-io/eli/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
 
-wget "https://github.com/cryon-io/eli/releases/download/$LATEST/eli-unix-$(uname -p)" -O eli &&
-    mv eli /usr/sbin/eli &&
+TMP_NAME="/tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+
+wget "https://github.com/cryon-io/eli/releases/download/$LATEST/eli-unix-$(uname -p)" -O "$TMP_NAME" &&
+    mv "$TMP_NAME" /usr/sbin/eli &&
     chmod +x /usr/sbin/eli &&
     echo "eli $LATEST for $(uname -p) successfuly installed."
