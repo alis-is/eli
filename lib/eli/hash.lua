d034de256271ed29a826c4e70fee88a592cbdfae
cryi <cryi@tutanota.com>
2023-05-22 20:52:05 +0200

    fix debug artifact and added missing sha init functions

--- lib/eli/hash.lua
+++ lib/eli/hash.lua
@@ -1,5 +1,5 @@
-local _hash = require "lmbed_hash"
-local _util = require "eli.util"
+local _hash = require"lmbed_hash"
+local _util = require"eli.util"
 
 ---@class HashGenerator
 ---#DES 'HashGenerator.ctx'
@@ -13,12 +13,12 @@ Sha256.__index = Sha256
 
 ---@deprecated
 function Sha256:new()
-    local sha256 = {}
+	local sha256 = {}
 
-    setmetatable(sha256, self)
-    self.__index = self
-    sha256.ctx = _hash.sha256init()
-    return sha256
+	setmetatable(sha256, self)
+	self.__index = self
+	sha256.ctx = _hash.sha256init()
+	return sha256
 end
 
 ---#DES 'hash.Sha256:update'
@@ -44,12 +44,12 @@ Sha512.__index = Sha512
 
 ---@deprecated
 function Sha512:new()
-    local sha512 = {}
+	local sha512 = {}
 
-    setmetatable(sha512, self)
-    self.__index = self
-    sha512.ctx = _hash.sha512init()
-    return sha512
+	setmetatable(sha512, self)
+	self.__index = self
+	sha512.ctx = _hash.sha512init()
+	return sha512
 end
 
 ---#DES 'hash.Sha512:update'
@@ -75,38 +75,16 @@ function Sha512:finish(hex) return self.ctx:finish(hex) end
 ---@return boolean
 ---@deprecated
 local function _hex_equals(hash1, hash2)
-    return _hash.equals(hash1, hash2, true)
+	return _hash.equals(hash1, hash2, true)
 end
 
-return _util.generate_safe_functions({
-    Sha256 = Sha256,
-    Sha512 = Sha512,
-    ---#DES hash.sha256sum
-    ---
-    ---@diagnostic disable-next-line: undefined-doc-param
-    ---@param data string
-    ---@diagnostic disable-next-line: undefined-doc-param
-    ---@param hex boolean?
-    ---@return string
-    sha256sum = _hash.sha256sum,
-    ---#DES hash.sha512sum
-    ---
-    ---@diagnostic disable-next-line: undefined-doc-param
-    ---@param data string
-    ---@diagnostic disable-next-line: undefined-doc-param
-    ---@param hex boolean?
-    ---@return string
-    sha512sum = _hash.sha512sum,
-    ---#DES 'hash.equals'
-    ---
-    ---Compares two strings (if hex true - compares as hex strings)
-    ---@diagnostic disable-next-line: undefined-doc-param
-    ---@param hash1 string
-    ---@diagnostic disable-next-line: undefined-doc-param
-    ---@param hash2 string
-    ---@diagnostic disable-next-line: undefined-doc-param
-    ---@param hex boolean?
-    ---@return boolean
-    equals = _hash.equals,
-    hex_equals = _hex_equals
-})
+return _util.generate_safe_functions{
+	Sha256 = Sha256,
+	Sha512 = Sha512,
+	sha256sum = _hash.sha256sum,
+	sha512sum = _hash.sha512sum,
+	sha256init = _hash.sha256init,
+	sha512init = _hash.sha512init,
+	equals = _hash.equals,
+	hex_equals = _hex_equals,
+}
