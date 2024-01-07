84b190c488d04fe4c0f910888ec70f5149c7384d
cryi <cryi@tutanota.com>
2024-01-08 00:04:07 +0100

    updated proc

--- lib/init.lua
+++ lib/init.lua
@@ -1,5 +1,5 @@
-ELI_LIB_VERSION = '0.32.0-dev.8'
-ELI_VERSION = '0.32.0-dev.8'
+ELI_LIB_VERSION = '0.32.0-dev.9'
+ELI_VERSION = '0.32.0-dev.9'
 do
 	local path = require"eli.path"
 	local _eos = require"eli.os"
