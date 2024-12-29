4c4f55d9bb7209b52268a602889c2bd561e98274
cryi <cryi@tutanota.com>
2024-12-29 14:50:42 +0100

    elify refactor

--- lib/eli/os.lua
+++ lib/eli/os.lua
@@ -1,11 +1,11 @@
-local is_os_extra_loaded, os_extra = pcall(require, "eli.os.extra")
+local is_loaded, os_extra = pcall(require, "eli.os.extra")
 local util = require"eli.util"
 
 local os = {
     ---#DES os.EOS
     ---
     ---@type boolean
-    EOS = is_os_extra_loaded,
+    EOS = is_loaded,
 }
 
-return is_os_extra_loaded and util.merge_tables(os, os_extra) or os
+return is_loaded and util.merge_tables(os, os_extra) or os
