69639b1dcea24e4a31cdbcc3ce41516373d7e134
v <cryi@tutanota.com>
2026-09-17 23:09:14 +0200

    patch cert download

--- tools/download-ca-certs.lua
+++
@@ -1,5 +0,0 @@
-local build = require"tools.util"
-local destination = assert(arg[1], "usage: tools/download-ca-certs.lua <destination>")
-local ok, err = build.download_ca_bundle(destination)
-
-assert(ok, err)
